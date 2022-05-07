44055ce2c32df1f8e288303aa97f3e4f13d72a40
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-07 01:19:47 -0400

    start of problem 2

---
+++ 2_addTwoNum.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // create a result ListNode default constructor
+        // keep track of the head.
+        
+        // first loop through the l1 and add all val into the ListNode
+        
+        // second loop through l2 and add val into the result ListNode
+        
+        // if the val is greater then 10, and next is not null
+            // add 1 to next node
+            // current val minus 10
+        
+        // else if next is null 
+            // create a next node with val = 1
+        
+        // return result
+    }
+}
