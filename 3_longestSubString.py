03ac336736e7a1ef200f3150ec5f483bcbaa9527
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-08 12:30:31 -0400

    learned another algorithm from youtube

--- 3_longestSubString.py
+++ 3_longestSubString.py
@@ -1,7 +1,21 @@
+from email import charset
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s):
+        charSet = set()
+        leftIndex = 0
+        longestLen = 0
         
-            
+        for i in range(len(s)):
+            # as long as there is duplicate remove from the left index
+            while s[i] in charSet:
+                charSet.remove(s[leftIndex])
+                leftIndex += 1
+            charSet.add(s[i])
+            longestLen = max(longestLen, (i - leftIndex+1))
+        
+        return longestLen
 
 if __name__ == "__main__":
     solve = Solution()
