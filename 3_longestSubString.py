02bded596faa91136c9dc3340db50eed3ce6a899
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-08 12:23:51 -0400

    redo problem 3

--- 3_longestSubString.py
+++ 3_longestSubString.py
@@ -1,22 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        currentList = []
-        longest = 0;
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if s[j] in currentList:
-                    if (longest < len(currentList)):
-                        longest = len(currentList)
-                    currentList = []
-                currentList.append(s[j])
-            if (longest < len(currentList)):
-                longest = len(currentList)
-            temp = currentList.pop()
-            if (temp != s[j]):
-                currentList = [temp]
-            else:
-                currentList = []
-        return longest
+        
             
 
 if __name__ == "__main__":
