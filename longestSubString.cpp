e26d9599bcf8c3dffffdc273071c18515dc35ea4
Steve Wufeng <pinkpineapple@freedom.csh.rit.edu>
2022-05-09 02:28:43 -0400

    compiled correctly for c++

--- longestSubString.cpp
+++ longestSubString.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
+#include<set>
 using namespace std;
 
 class Solution
 {
     public:
-        Solution();
-        int lengthOfLongestSubstring(string s) {
-        
+        Solution(){}
+        int lengthOfLongestSubstring(string s) 
+		{	
+			set<char> window = {};
+			int left_index = 0;
+			int longest_len = 0;
+
+			for (int r_index = 0; r_index < s.length(); r_index++)
+			{
+				while (window.find(s[r_index]) != window.end())
+				{
+					window.erase(s[left_index]);
+					left_index++;
+				}
+				window.insert(s[r_index]);
+				longest_len = (longest_len > window.size()) ? longest_len : window.size();
+			}
+			return longest_len;
         }
 };
 
