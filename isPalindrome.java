1a7811b8abb5b1107008231832e0c362f41d1612
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-09 23:53:40 -0400

    added java code for problem 9

---
+++ isPalindrome.java
@@ -0,0 +1,17 @@
+// problem 9
+public class isPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        else {
+            String s = "" + x;
+            for (int i = 0; i < s.length()/2; i++) {
+                if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
