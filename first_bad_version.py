a6b22bea5ff3b2dade4f00e086911bf34919aa80
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-12 18:14:25 -0400

    finished first_bad Version.py

--- first_bad_version.py
+++ first_bad_version.py
@@ -1,19 +1,22 @@
+# problem 278
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         # start searching from middle
-        index = n//2
-        # if the middle is not a bad version, increment index
-        previousBad = isBadVersion(index-1)
-        bad = isBadVersion(index)
+        index = n
+        # surround in while loop
+        while (True):
+            # if the middle is not a bad version, increment index
+            previousBad = isBadVersion(index-1)
+            bad = isBadVersion(index)
         
-        if (!previousBad and bad):
-            return index
-        if (not bad):
-            index += index//2
-        # else if is a bad version, decrement index
-        elif bad:
-            index -= index//2
+            if (not previousBad and bad):
+                return index
+            if (not bad):
+                index += index//2
+            # else if is a bad version, decrement index
+            elif bad:
+                index -= index//2
         
