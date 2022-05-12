77ddcf81d2fa2b85bf72eabdc7a59f7e416d4a8b
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-05-12 18:19:46 -0400

    debuging code in vsCode

--- first_bad_version.py
+++ first_bad_version.py
@@ -1,16 +1,17 @@
 # problem 278
 # The isBadVersion API is already defined for you.
-# def isBadVersion(version: int) -> bool:
+def isBadVersion(version: int, bad):
+    return version >= bad
 
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
+    def firstBadVersion(self, n: int, bad):
         # start searching from middle
         index = n
         # surround in while loop
         while (True):
             # if the middle is not a bad version, increment index
-            previousBad = isBadVersion(index-1)
-            bad = isBadVersion(index)
+            previousBad = isBadVersion(index-1, bad)
+            bad = isBadVersion(index, bad)
         
             if (not previousBad and bad):
                 return index
@@ -19,4 +20,8 @@ class Solution:
             # else if is a bad version, decrement index
             elif bad:
                 index -= index//2
-        
+
+if __name__ == '__main__':
+    solve = Solution()
+    x = solve.firstBadVersion(5, 5)
+    print(x)
